09c668ad0b829898c1e01ad19f984010094fa697
nidroy <62301362+nidroy@users.noreply.github.com>
2022-01-10 00:16:36 +0300

    creating a database
    начало создания запросов к бд и подключение библиотеки бд к unity

---
+++ Unknown World of Mystery/Assets/Scripts/Database/Database.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Database : MonoBehaviour
+{
+    // ад SQL Server
+    public static string connectionString = "Data Source=MYCOMPUTER;Initial Catalog='Unknown World of Mystery database';Integrated Security=True";
+}
