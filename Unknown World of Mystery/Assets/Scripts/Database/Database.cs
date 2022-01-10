ffc935f86dc037165811fc40c69500cea31476df
nidroy <62301362+nidroy@users.noreply.github.com>
2022-01-11 00:01:07 +0300

    removing excess
    удаление лишних файлов. Подготовка к клиент-серверу

--- Unknown World of Mystery/Assets/Scripts/Database/Database.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Database : MonoBehaviour
-{
-    // ад SQL Server
-    public static string connectionString = "Data Source=MYCOMPUTER;Initial Catalog='Unknown World of Mystery database';Integrated Security=True";
-}
--- Unknown World of Mystery/Assets/Scripts/Database/IRequest.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IRequest
-{
-    public string Execute(string connectionString);
-}
